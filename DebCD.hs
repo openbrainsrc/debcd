f942d2bb61b70730cb38b001bfa5954954c7b0e9
Tom Nielsen <tanielsen@gmail.com>
2014-02-20 15:47:54 +0000

    tests, debianiser

--- DebCD.hs
+++ DebCD.hs
@@ -4,6 +4,7 @@ module Main where
 
 import qualified Data.Yaml.Config as YConf
 import System.Environment
+import System.Directory
 import System.Exit
 
 import qualified Data.Text as T
@@ -29,9 +30,10 @@ main = do
 
  -- update 
  case upRepos of
-   "all" -> void $ system "apt-get update"
+{-   "all" -> void $ system "apt-get update"
    "none" -> return ()   
-   _ -> void $ system "apt-get update"
+   _ -> void $ system "apt-get update" -}
+   _ -> return ()
 
  upgradesAvailable <- do ex <- system "apt-get -u upgrade --assume-no"
                          return $ ex /= ExitSuccess 
@@ -40,7 +42,7 @@ main = do
 
  -- upgrade 
  when upgradesAvailable $ upgrade sender
-
+ upgrade sender
 
 upgrade sender = do
 
@@ -48,21 +50,42 @@ upgrade sender = do
  void $ system "apt-get upgrade"
 
  -- run tests
+ testsOK <- runTests 
 
  -- roll back on failure
+ when (not testsOK) $ rollback selections
 
  return ()
+
+-- these two based on http://www.debian-administration.org/article/669/
+
+runTests = do
+  files <- getDirectoryContents "/etc/debcd/tests.d/"
+  fmap and $ forM files $ \file -> do
+    p <- getPermissions $ "/etc/debcd/tests.d/"++file
+    if not $ executable p
+       then return True
+       else do res <- system $ "/etc/debcd/tests.d/"++file
+               case res of 
+                 ExitSuccess -> return True
+                 ExitFailure _ -> return False
 
 createFreezeList :: IO String
 createFreezeList = do
  now <- getCurrentTime
  let nowS =  DTF.formatTime defaultTimeLocale "%Y%m%d%H%M" now
- -- create dpkg freeze file. http://www.debian-administration.org/article/669/
  system "mkdir -p /var/debcd"
  system $ "aptitude -q -F \"%?p=%?V %M\" --disable-columns search \\~i > /var/debcd/"
           ++nowS
  return nowS
 
+rollback :: String -> IO ()
+rollback list = void $ do
+  system $ "aptitude -q -R --schedule-only install $(awk < /var/debcd/"++
+           list++" '{print $1}')"
+  system $ "aptitude -q -R --schedule-only markauto $(awk < /var/debcd/"++
+           list++" '$2==\"A\" {split($1,A,\"=\");print A[1]}')"
+  system $ "aptitude -y -o Dpkg::Options::=\"--force-confdef\" install"
 
 getConfig :: IO YConf.Config
 getConfig = do
--- Makefile
+++ Makefile
@@ -1,6 +1,18 @@
-cibuild: 
-	cabal sandbox init
-	cabal install
+cibuild: cabalinstall deb
 
 citest:
 	echo "No tests"
+
+cabalinstall:
+	cabal sandbox init
+	cabal install
+
+deb:
+	rm -rf install_root
+	mkdir -p install_root/usr/sbin
+	mkdir -p install_root/etc/debcd/tests.d
+
+	cp .cabal-sandbox/bin/debcd install_root/usr/sbin/
+	cp pass install_root/etc/debcd/tests.d/
+	cp debconf.yml.example install_root/etc/debcd/
+	cd install_root && fpm -s dir -t deb -n debcd -v 0.1.`date +%Y%m%d%H%M` --prefix / .
